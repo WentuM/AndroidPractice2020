03622322cdf5d9773b381df3177860460bf241b7
WentuM <kesand@mail.ru>
2020-10-16 21:29:03 +0300

    task/hw4_final_homework

---
+++ app/src/main/java/com/example/androidpractice2020/FifthFragment.kt
@@ -0,0 +1,17 @@
+package com.example.androidpractice2020
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FifthFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.photo5_fragment, container, false)
+    }
+}
